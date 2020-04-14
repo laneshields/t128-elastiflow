b6c92bf9962f7dd5b4b89294b5e7df07a908ab5d
Lane Shields <lshields@128technology.com>
2020-04-14 02:48:16 +0000

    Removed unneeded python code

--- elastiflow-logstash-oss-128t/push-template.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-import requests
-
-with open('elastiflow.template.json') as fd:
-  template = fd.read()
-
-headers = {'Content-Type':'application/json'}
-resp = requests.put('http://localhost:9200/_template/elastiflow', headers=headers, data=template)
